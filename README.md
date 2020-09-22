e385a6ea0a015d25292be67e6d55ac1f30b01deb
Julian Pollinger <neferin42@gmail.com>
2020-09-22 22:50:51 +0200

    start

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chuck Norris Bot for Discord
+If "chuck" or "chuckNorris" is written in the Chat, this bot will reply with a random chuck Norris Joke
---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+const axios = require("axios")
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    console.log("message recieved")
+    const c = msg.content.toLowerCase();
+    if (c === 'chuck' || c === 'chuckNorris') {
+        axios.get("https://api.icndb.com/jokes/random").then(res => {
+            msg.channel.send(res.data.value.joke);
+            console.log("Witz gesendet")
+        }).catch(() => {
+            axios.get("https://api.chucknorris.io/jokes/random").then(resb => {
+                msg.channel.send(resb.data.value);
+                console.log("Backupwitz gesendet");
+            }).catch(err => {
+                console.error(err);
+                msg.channel.send("I wasn't able to get this joke. Probably Cuck Norris doesn't want you to laugh about him. Try again later!");
+            });
+        });
+    }else if (c === 'ping') {
+        msg.channel.send("pong");
+        console.log("pong")
+    }
+});
