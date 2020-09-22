c2948e628096c1c732f96c101d33c68eec8a9c59
Julian Pollinger <neferin42@gmail.com>
2020-09-22 23:53:25 +0200

    eslint

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
-const axios = require("axios")
+const { TOKEN } = process.env;
+const axios = require('axios');
 
 bot.login(TOKEN);
 
@@ -10,24 +11,24 @@ bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-    console.log("message recieved")
+bot.on('message', (msg) => {
+    console.log('message recieved');
     const c = msg.content.toLowerCase();
     if (c === 'chuck' || c === 'chuckNorris') {
-        axios.get("https://api.icndb.com/jokes/random").then(res => {
+        axios.get('https://api.icndb.com/jokes/random').then((res) => {
             msg.channel.send(res.data.value.joke);
-            console.log("Witz gesendet")
+            console.log('Witz gesendet');
         }).catch(() => {
-            axios.get("https://api.chucknorris.io/jokes/random").then(resb => {
+            axios.get('https://api.chucknorris.io/jokes/random').then((resb) => {
                 msg.channel.send(resb.data.value);
-                console.log("Backupwitz gesendet");
-            }).catch(err => {
+                console.log('Backupwitz gesendet');
+            }).catch((err) => {
                 console.error(err);
                 msg.channel.send("I wasn't able to get this joke. Probably Cuck Norris doesn't want you to laugh about him. Try again later!");
             });
         });
-    }else if (c === 'ping') {
-        msg.channel.send("pong");
-        console.log("pong")
+    } else if (c === 'ping') {
+        msg.channel.send('pong');
+        console.log('pong');
     }
 });
