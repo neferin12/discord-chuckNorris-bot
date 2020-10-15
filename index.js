8fd574d06744e4e639839c73839f1086e3a9fec8
Julian <neferin42@gmail.com>
2020-10-15 22:17:29 +0200

    restructering

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-require('dotenv').config();
+require('dotenv')
+    .config();
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 const { TOKEN } = process.env;
 const axios = require('axios');
+
+const PREFIX = '$';
+const ALLOW_NO_PREFIX = true;
 
 bot.login(TOKEN);
 
@@ -11,24 +15,36 @@ bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', (msg) => {
-    console.log('message recieved');
-    const c = msg.content.toLowerCase();
+function answer(c, msg) {
     if (c === 'chuck' || c === 'chuckNorris') {
-        axios.get('https://api.icndb.com/jokes/random').then((res) => {
-            msg.channel.send(res.data.value.joke);
-            console.log('Witz gesendet');
-        }).catch(() => {
-            axios.get('https://api.chucknorris.io/jokes/random').then((resb) => {
-                msg.channel.send(resb.data.value);
-                console.log('Backupwitz gesendet');
-            }).catch((err) => {
-                console.error(err);
-                msg.channel.send("I wasn't able to get this joke. Probably Cuck Norris doesn't want you to laugh about him. Try again later!");
+        axios.get('https://api.icndb.com/jokes/random')
+            .then((res) => {
+                msg.channel.send(res.data.value.joke);
+                console.log('Witz gesendet');
+            })
+            .catch(() => {
+                axios.get('https://api.chucknorris.io/jokes/random')
+                    .then((resb) => {
+                        msg.channel.send(resb.data.value);
+                        console.log('Backupwitz gesendet');
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                        msg.channel.send('I wasn\'t able to get this joke. Probably Cuck Norris doesn\'t want you to laugh about him. Try again later!');
+                    });
             });
-        });
     } else if (c === 'ping') {
         msg.channel.send('pong');
         console.log('pong');
+    }
+}
+
+bot.on('message', (msg) => {
+    console.log('message recieved');
+    const c = msg.content.toLowerCase();
+    if (ALLOW_NO_PREFIX) answer(c, msg);
+    if (c[0] === PREFIX) {
+        const substring = c.substring(1, c.length);
+        answer(substring, msg);
     }
 });
